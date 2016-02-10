81a3e18a2e42a0d5d6ad6de25be4810422badbf4
Martin Jespersen <mjatzbox@gmail.com>
2016-02-10 20:33:54 +0100

    wp update working

--- wp-dev-helpers.php
+++ wp-dev-helpers.php
@@ -11,57 +11,36 @@ Description: What does your plugin do and what features does it offer...
 ?>
 <?php
 class WPDevHelpers {
-    
-    // Dribbble username or user id
-    protected $user;
-     
-    public function __construct($user)
+         
+    public function __construct()
     {
-        $this->user = $user;
-    }
-     
-    // public function getPlayerShots($perPage = 15)
-    // {
-    //     $user = $this->user;
-         
-    //     //$json = wp_remote_get($this->apiUrl . 'players/' . $user . '/shots?per_page=' . $perPage);
-         
-    //     $array = json_decode($json['body']);
-         
-    //     $shots = $array->shots;
-         
-    //     return $shots;
-    // }
-
-
-    /* -- Debugging functions ----------------------*/
-
-
-    // Write to the Wordpress debug.log
-	public function write_log ( $log )  {
-		if ( is_array( $log ) || is_object( $log ) ) {
-		 	error_log( print_r( $log, true ) );
-		} else {
-		 	error_log( $log );
-		}
-	}
-
-    // display PHP vars in console ( doesnt work with ajax calls ) 
-    public function print_js( $var ){ 
-		$var = json_encode($var ); ?>
-		<script type="text/javascript">
-			console.dir( <?php echo $var ?> ); 
-		</script>
-		<?php 
-    }
-
-    // display print_r with <pre> tags 
-    public function print_pre( $var ){
-		echo '<pre>';
-		print_r( $var );
-		echo '</pre>';
+        
     }
 }
 
+function write_log ( $log )  {
+	if ( is_array( $log ) || is_object( $log ) ) {
+	 	error_log( print_r( $log, true ) );
+	} else {
+	 	error_log( $log );
+	}
+}
+
+// display PHP vars in console ( doesnt work with ajax calls ) 
+function print_js( $var ){ 
+	$var = json_encode($var ); ?>
+	<script type="text/javascript">
+		console.dir( <?php echo $var ?> ); 
+	</script>
+	<?php 
+}
+
+    // display print_r with <pre> tags 
+function print_pre( $var ){
+	echo '<pre>';
+	print_r( $var );
+	echo '</pre>';
+}
+ 
 
 ?>
